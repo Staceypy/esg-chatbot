8952bd2494b6762cf6c4026c41913305caf953ac
Staceypy <13625410672@163.com>
2025-01-18 15:37:51 +0100

    initial commit

---
+++ run_llama.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+from llama_index.core.tools import FunctionTool
+
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings, QueryEngineTool
+
+Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
+
+documents = SimpleDirectoryReader("./data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+
+response = query_engine.query(
+    "What was the total amount of the 2023 Canadian federal budget?"
+)
+print(response)
+
+budget_tool = QueryEngineTool.from_defaults(
+    query_engine,
+    name="canadian_budget_2023",
+    description="A RAG engine with some basic facts about the 2023 Canadian federal budget.",
+)
+
+agent = ReActAgent.from_tools(
+    [multiply_tool, add_tool, budget_tool], verbose=True
+)
+
+response = agent.chat(
+    "What is the total amount of the 2023 Canadian federal budget multiplied by 3? Go step by step, using a tool to do any math."
+)
+
+print(response)
